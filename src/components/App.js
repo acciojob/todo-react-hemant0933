534a1feb88121f72ad6991093e7d78e384db1038
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-06-22 23:10:14 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text,setText] = useState("");
+  const [data,setData] = useState([]);
+
+  const addToList = () => {
+    if(text.trim() !== ''){
+      setData([...data, text]);
+      setText("");
+    }
+    
+  }
+  const handleDelete = (index) => {
+    const updatedItems = [...data];
+    updatedItems.splice(index,1);
+    setData(updatedItems);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>To-Do List</h1>
+        <div className="container">
+          <input type="text" placeholder="Add To List" value={text} onChange={(e) => setText(e.target.value)} />
+          <button onClick={addToList}>Add</button>
+        </div>
+
+        <ul className="list-container">
+          {data.length > 0 && data.map((item,index)=>(
+            <li key={index}>
+              {item}
+              <button onClick={()=>handleDelete(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+
     </div>
   )
 }
