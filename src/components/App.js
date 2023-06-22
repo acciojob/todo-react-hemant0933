9f8b8713760976eccdf9b63241eb7179506c6dc5
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-06-22 23:35:46 +0530

    submitting code-Thu Jun 22 2023 23:35:44 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -23,15 +23,17 @@ const App = () => {
         <h1>To-Do List</h1>
         <div className="container">
           <input type="text" placeholder="Add To List" value={text} onChange={(e) => setText(e.target.value)} />
-          <button onClick={addToList}>Add</button>
+          <button onClick={addToList}>Add Todo</button>
         </div>
 
         <ul className="list-container">
           {data.length > 0 && data.map((item,index)=>(
-            <li key={index}>
-              {item}
+            
+              <li style={{display:'flex',justifyContent:'space-between'}} key={index}>
+              {item} 
               <button onClick={()=>handleDelete(index)}>Delete</button>
             </li>
+           
           ))}
         </ul>
 
